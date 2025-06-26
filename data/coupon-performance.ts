6c7092690321e16291581957ff4491cd69a5550d
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2025-06-27 00:51:42 +0100

    chore: create mock data for coupon table

---
+++ data/coupon-performance.ts
@@ -0,0 +1,10 @@
+export const coupons = [
+  { couponCode: "FLASHJUNE24", usageCount: 43 },
+  { couponCode: "FREESHIPPING", usageCount: 14 },
+  { couponCode: "WELCOME1000", usageCount: 35 },
+  { couponCode: "JUNE10OFF", usageCount: 16 },
+  { couponCode: "LOYALTY300", usageCount: 12 },
+  { couponCode: "NEWCUSTOMER10", usageCount: 8 },
+  { couponCode: "MAYFLASH500", usageCount: 68 },
+  { couponCode: "BULKBUY5", usageCount: 16 },
+];
